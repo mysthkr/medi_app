c4d520ecde4438aa0e2f42b962e140a810e0d190
栗田大輔 <daikons.msn36@gmail.com>
2024-02-13 00:50:03 +0900

    応募画面のバックエンド実装

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,14 @@
 class Candidate < ApplicationRecord
+  validates :full_name, presence: true 
+  validates :full_name_kana, presence: true 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX}, 
+                    uniqueness: true
+  validates :email, presence: true 
+  VALID_PHONE_NUMBER__REGEX = /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  validates :phone_number, presence: true,
+                           format: { with: VALID_PHONE_NUMBER__REGEX},
+                           uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
